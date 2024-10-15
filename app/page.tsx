d4c05cb43e5b87b55b0b8fa017d68aed155e5fc8
Trần Mạnh Tuấn <tmtuan120704@gmail.com>
2024-10-15 08:47:53 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,10 @@ export default function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m James
+            Hello, I&apos;m Tuan
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-            Welcome to my blog template. Built using tailwind, shadcn, velite
-            and Nextjs 14.
+            Welcome to my blog where i start to become a SOC analyst
           </p>
           <div className="flex flex-col gap-4 justify-center sm:flex-row">
             <Link
